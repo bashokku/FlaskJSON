fae79dbe5c5342d2e3e1ba4068a203be2b1ccbbc
GoCD User <go@master>
2017-06-19 15:53:01 +0000

    new branch

--- agent.sh
+++ agent.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-echo "from master"
-echo "from master"
-echo "from master"
-echo "from master"
-echo "from master"
-echo "from master"
-echo "from master"
-echo "from master"
-
+echo "from dev"
 docker build --no-cache --tag bashokku/flaskapp:latest --file Dockerfile .
 docker tag flaskapp bashokku/flaskapp
 docker push bashokku/flaskapp
