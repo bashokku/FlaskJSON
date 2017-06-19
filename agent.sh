8730954903f2ae325a20087621eb7e5468e1618b
GoCD User <go@master>
2017-06-19 16:41:45 +0000

    added to dev branch

--- agent.sh
+++ agent.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "from dev"
+echo " from dev"
 docker build --no-cache --tag bashokku/flaskapp:latest --file Dockerfile .
 docker tag flaskapp bashokku/flaskapp
 docker push bashokku/flaskapp
