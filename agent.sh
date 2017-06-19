9459628522248357835d88b8f8a23563a9617388
GoCD User <go@master>
2017-06-19 17:55:23 +0000

    dev1a

--- agent.sh
+++ agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "from dev1"
-###echo "from dev1 dev1 "
+####echo "from dev1 dev1 "
 docker build --no-cache --tag bashokku/flaskapp:latest --file Dockerfile .
 docker tag flaskapp bashokku/flaskapp
 docker push bashokku/flaskapp
