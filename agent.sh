d66f11ebf2cbb0a05cfcc2f829dc22c8d71bc992
GoCD User <go@master>
2017-06-19 16:52:48 +0000

    testing

--- agent.sh
+++ agent.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "from test"
 echo "from test"
+echo "test test"
 docker build --no-cache --tag bashokku/flaskapp:latest --file Dockerfile .
 docker tag flaskapp bashokku/flaskapp
 docker push bashokku/flaskapp
